4d8f4df23ee3f7024df678da08c0d54ffc5fbb58
Dhruv <dhruvramani98@gmail.com>
2019-04-20 17:14:00 +0530

    Base

---
+++ preprocessor/reader.py
@@ -0,0 +1,235 @@
+"""
+reader.py
+
+Core script containing preprocessing logic - reads bAbI Task Story, and returns
+vectorized forms of the stories, questions, and answers.
+"""
+import re
+import os
+import json
+import bcolz
+import pickle
+import numpy as np
+from tqdm import tqdm
+from collections import OrderedDict
+
+FORMAT_STR = "qa%d_"
+PAD_ID = 0
+SPLIT_RE = re.compile('(\W+)?')
+DATA_TYPES = ['train', 'valid', 'test']
+
+_DIR = "/home/nevronas/Projects/Personal-Projects/Dhruv/NeuralDialog-CVAE/data/commonsense/"
+_GLOVE_PATH = '/home/nevronas/word_embeddings/glove_twitter'
+_EMB_DIM = 100
+_MAX_WLEN = 18
+_VOCAB = -1
+
+pickle_path = _DIR + "data.pkl"
+partition_path = _DIR + "storyid_partition.txt"
+annotation_path = _DIR + "json_version/annotations.json" 
+
+classes = ["joy", "trust", "fear", "surprise", "sadness", "disgust", "anger", "anticipation"]
+
+def init_glove(glove_path=_GLOVE_PATH): # Run only first time
+    words = []
+    idx = 0
+    word2idx = {}
+    vectors = bcolz.carray(np.zeros(1), rootdir='{}/27B.{}d.dat'.format(glove_path, _EMB_DIM), mode='w')
+    with open('{}/glove.twitter.27B.{}d.txt'.format(glove_path, _EMB_DIM), 'rb') as f:
+        for l in f:
+            line = l.decode().split()
+            word = line[0]
+            words.append(word)
+            word2idx[word] = idx
+            idx += 1
+            vect = np.array(line[1:]).astype(np.float)
+            vectors.append(vect)
+
+    vectors = bcolz.carray(vectors.reshape((1193514, _EMB_DIM)), rootdir='{}/27B.{}.dat'.format(glove_path, _EMB_DIM), mode='w')
+    vectors.flush()
+    pickle.dump(words, open('{}/27B.{}_words.pkl'.format(glove_path, _EMB_DIM), 'wb'))
+    pickle.dump(word2idx, open('{}/27B.{}_idx.pkl'.format(glove_path, _EMB_DIM), 'wb'))
+    return idx
+
+def tokenize(sentence):
+    "Tokenize a string by splitting on non-word characters and stripping whitespace."
+    return [token.strip().lower() for token in re.split(SPLIT_RE, sentence) if token.strip()]
+
+def load_glove():
+    global _VOCAB
+    vectors = bcolz.open('{}/27B.{}.dat'.format(_GLOVE_PATH, _EMB_DIM))[:]
+    words = pickle.load(open('{}/27B.{}_words.pkl'.format(_GLOVE_PATH, _EMB_DIM), 'rb'))
+    word2idx = pickle.load(open('{}/27B.{}_idx.pkl'.format(_GLOVE_PATH, _EMB_DIM), 'rb'))
+
+    if(_VOCAB == -1):
+        _VOCAB = len(words)
+
+    return {w: vectors[word2idx[w]] for w in words}
+
+def get_labels(charay):
+        ann = []
+        for i in range(3):
+            try :
+                ann.append(charay["emotion"]["ann{}".format(i)]["plutchik"])
+            except:
+                print("ann{} ignored".format(i))
+
+        if(len(ann) == 0): # NOTE - change this maybe
+            return [0 for _ in classes]
+
+        final_dict = dict()
+        for classi in classes:
+            final_dict[classi] = [1, 1, 1]
+
+        for idx in range(len(ann)):
+            for i in ann[idx]:
+                if(i[:-2] in final_dict.keys()):
+                    final_dict[i[:-2]][idx] = int(i[-1])
+
+        majority = []
+        for key in final_dict.keys():
+            if(int(sum(final_dict[key]) / 3) >= 2):
+                majority.append(key) #[key if(floor(sum(final_dict[key]) / 3) >= 2) for key in final_dict.keys()]
+
+        onehot = [1 if i in majority else 0 for i in classes]
+        return onehot
+
+def create_dataset(load=True, data_type="test"):
+    global annotation_path, partition_path
+    data_type = "valid" if data_type == "train" else data_type
+    annotation_file = open(annotation_path, "r")
+    raw_data = json.load(annotation_file, object_pairs_hook=OrderedDict)
+    glove = load_glove()
+
+    text_arr, all_labels, char_arr, mask_arr = [], [], [], []
+    stories_dat = []
+    with open(partition_path, "r") as partition_file:
+        for line in partition_file:
+            id_key = line.split("\t")[0]
+            story = raw_data[id_key]
+            
+            if(story["partition"] != data_type):
+                continue 
+
+            sentences = story["lines"]
+            characters = sentences['1']["characters"]
+            
+            s_dim, c_dim, count = len(sentences.keys()), len(characters.keys()), 0
+            mask_dim = s_dim * c_dim
+            embeddings, labels, mask = [], [], [0] * mask_dim
+
+            for si in range(s_dim):
+                sent = sentences[str(si + 1)]
+                text = sent["text"]
+                
+                embed_string = re.sub(r"[^a-zA-Z]+", ' ', text)
+                embedding = [glove.get(word, glove['unk']) for word in embed_string.split(" ")]
+                embeddings.append(embedding)
+                
+                charecs = list(sent["characters"].keys())
+                labels.append([])
+                for cj in range(c_dim):
+                    char = sent["characters"][charecs[cj]]
+                    one_hot = get_labels(char)
+                    labels[si].append([])
+                    labels[si][cj] = one_hot
+                    if(1 in one_hot):
+                        mask[count] = 1
+                    count += 1
+
+
+            mask = np.asarray(mask)
+            labels = np.asarray(labels)
+            embeddings = np.asarray(embeddings)
+            labels = labels.reshape(labels.shape[0] * labels.shape[1], labels.shape[2])
+            
+            all_labels.append(labels)   # Shape : [stories, s_d * c_d, labels_dim]
+            mask_arr.append(mask)       # Shape : [stories, s_d * c_d]
+            text_arr.append(embeddings) # Shape : [stories, s_d, words, embedding_dim]
+            char_arr.append(c_dim)      # Shape : [stories, 1]  - No. of chars. - to find upper bound
+            
+            # OR - decide
+            #stories_dat.append((embeddings, labels, mask, c_dim))
+
+    return text_arr, all_labels, mask_arr, char_arr # stories_dat # - ALL ARE LISTS
+
+def parse():
+    text_arr, all_labels, mask_arr, char_arr = create_dataset()
+    
+    dataset_size = len(text_arr)
+    sentence_lengths = [story.shape[0] for story in text_arr]
+    word_lengths = [len(story[ss]) for story in text_arr for ss in range(story.shape[0])]
+
+    max_sentence_length = max(sentence_lengths)
+    max_word_length = max(word_lengths)
+    max_char_length = max(char_arr)
+
+    print(max_sentence_length, max_word_length, max_char_length)
+    mask_dim, labels_dim = len(all_labels[0]), len(all_labels[0][0])
+    embedding_dim = _EMB_DIM
+
+    text_arr, all_labels, mask_arr = pad_stories(text_arr, all_labels, mask_arr, max_sentence_length, max_word_length, max_char_length)
+    #stories_train = (text_arr, all_labels, mask_arr)
+    print(text_arr.shape, all_labels.shape, mask_arr.shape)
+
+def pad_stories(text_arr, all_labels, mask_arr, max_sentence_length, max_word_length, max_char_length):
+    
+    for i in range(len(text_arr)):
+        story = text_arr[i]
+        shape = story.shape
+        sentence_pad = max_sentence_length - shape[0]
+        # FML
+        new_story = []
+        story_type = type(story[0])
+        if(story_type != list):
+            for j in range(shape[0]):
+                a = story[j].tolist()
+                new_story.append(a)
+        for j in range(shape[0]):
+            if(len(story[j]) != max_word_length):
+                if(story_type != list):
+                    a = new_story[j]
+                    for i in range(max_word_length - len(a)):
+                        a.append([0] * _EMB_DIM)
+                    new_story[j] = np.asarray(a)
+                else :
+                    story[j] = story[j] + [[0] * _EMB_DIM] * (max_word_length - len(list(story[j])))
+                    story[j] = np.asarray(story[j])
+        if(story_type != list):
+            story = np.asarray([new_story[k] for k in range(shape[0])])
+        else:
+            story = np.asarray([story[k] for k in range(shape[0])])
+        if(sentence_pad != 0):
+            text_arr[i] = np.pad(story, ((0, sentence_pad), (0, 0), (0, 0)), 'constant')
+        else :
+            text_arr[i] = story
+
+    for i in range(len(text_arr)):
+        if(text_arr[i].shape[1] != max_word_length):
+            pad_length = max_word_length - text_arr[i].shape[1]
+            text_arr[i] = np.pad(text_arr[i], ((0, 0), (0, pad_length), (0,0)), 'constant')
+    
+    for i in range(len(all_labels)):
+        label = all_labels[i]
+        shape = label.shape
+        pad_length = max_sentence_length * max_char_length - shape[0]
+        all_labels[i] = np.pad(label, ((0, pad_length), (0,0)), 'constant')
+
+    for i in range(len(mask_arr)):
+        mask = mask_arr[i]
+        shape = mask.shape
+        pad_length = max_sentence_length * max_char_length - shape[0]
+        mask_arr[i] = np.pad(mask, ((0, pad_length)), 'constant')
+
+    mask_arr = np.asarray(mask_arr)     # Shape : [max_sentence_length, s_d * c_d]
+    text_arr = np.asarray(text_arr)     # Shape : [max_sentence_length, max_word_length, embedding_dim]
+    all_labels = np.asarray(all_labels) # Shape : [max_sentence_length, s_d * c_d, labels_dim]
+
+    return text_arr, all_labels, mask_arr
+
+
+def tokenize(sentence):
+    """
+    Tokenize a string by splitting on non-word characters and stripping whitespace.
+    """
+    return [token.strip().lower() for token in re.split(SPLIT_RE, sentence) if token.strip()]
---
+++ run.py
@@ -0,0 +1,140 @@
+"""
+run.py
+
+Core script for building, training, and evaluating a Recurrent Entity Network.
+"""
+from model.entity_network import EntityNetwork
+from preprocessor.reader import parse
+import datetime
+import os
+import pickle
+import tensorflow as tf
+import tflearn
+
+FLAGS = tf.app.flags.FLAGS
+
+# Run Details
+tf.app.flags.DEFINE_integer("task_id", 1, "ID of Task to Train/Evaluate [1 - 20].")
+tf.app.flags.DEFINE_string("data_path", "tasks/en-valid-10k", "Path to Training Data")
+
+# Model Details
+tf.app.flags.DEFINE_integer("embedding_size", 100, "Dimensionality of word embeddings.")
+tf.app.flags.DEFINE_integer("memory_slots", 20, "Number of dynamic memory slots.")
+
+# Training Details
+tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size for training/evaluating.")
+tf.app.flags.DEFINE_integer("num_epochs", 200, "Number of Training Epochs.")
+tf.app.flags.DEFINE_float("learning_rate", .01, "Learning rate for ADAM Optimizer.")
+tf.app.flags.DEFINE_integer("decay_epochs", 25, "Number of epochs to run before learning rate decay.")
+tf.app.flags.DEFINE_float("decay_rate", 0.5, "Rate of decay for learning rate.")
+tf.app.flags.DEFINE_float("clip_gradients", 40.0, 'Norm to clip gradients to.')
+
+# Logging Details
+tf.app.flags.DEFINE_integer("validate_every", 10, "Validate every validate_every epochs.")
+tf.app.flags.DEFINE_float("validation_threshold", .95, "Validation threshold for early stopping.")
+
+def main(_):
+    # Get Vectorized Forms of Stories, Questions, and Answers
+    train, val, test, word2id = parse(FLAGS.data_path, FLAGS.task_id)
+    trainS, trainS_len, trainQ, trainA, _ = train
+    valS, valS_len, valQ, valA, _ = val
+    testS, testS_len, testQ, testA, _ = test
+
+    # Assert Shapes
+    assert(trainS.shape[1:] == valS.shape[1:] == testS.shape[1:])
+    assert(trainQ.shape[1] == valQ.shape[1] == testQ.shape[1])
+
+    # Setup Checkpoint + Log Paths
+    ckpt_dir = "./checkpoints/qa_%d/" % FLAGS.task_id
+    if not os.path.exists(ckpt_dir):
+        os.mkdir(ckpt_dir)
+    
+    # Build Model
+    with tf.Session() as sess:
+        # Instantiate Model
+        entity_net = EntityNetwork(word2id, trainS.shape[2], trainS.shape[1], FLAGS.batch_size,
+                                   FLAGS.memory_slots, FLAGS.embedding_size, FLAGS.learning_rate, 
+                                   FLAGS.decay_epochs * (trainS.shape[0]/FLAGS.batch_size), FLAGS.decay_rate)
+        
+        # Initialize Saver
+        saver = tf.train.Saver()
+
+        # Initialize all Variables
+        if os.path.exists(ckpt_dir + "checkpoint"):
+            print 'Restoring Variables from Checkpoint!'
+            saver.restore(sess, tf.train.latest_checkpoint(ckpt_dir))
+            with open(ckpt_dir + "training_logs.pik", 'r') as f:
+                train_loss, train_acc, val_loss, val_acc = pickle.load(f)
+        else:
+            print 'Initializing Variables!'
+            sess.run(tf.global_variables_initializer())
+            train_loss, train_acc, val_loss, val_acc = {}, {}, {}, {}
+        
+        # Get Current Epoch
+        curr_epoch = sess.run(entity_net.epoch_step)
+
+        # Start Training Loop
+        n, val_n, test_n, bsz, best_val = trainS.shape[0], valS.shape[0], testS.shape[0], FLAGS.batch_size, 0.0
+        for epoch in range(curr_epoch, FLAGS.num_epochs):
+            loss, acc, counter = 0.0, 0.0, 0
+            for start, end in zip(range(0, n, bsz), range(bsz, n, bsz)):
+                curr_loss, curr_acc, _ = sess.run([entity_net.loss_val, entity_net.accuracy, entity_net.train_op], 
+                                                  feed_dict={entity_net.S: trainS[start:end], 
+                                                             entity_net.S_len: trainS_len[start:end],
+                                                             entity_net.Q: trainQ[start:end],
+                                                             entity_net.A: trainA[start:end]})
+                loss, acc, counter = loss + curr_loss, acc + curr_acc, counter + 1
+                if counter % 100 == 0:
+                    print "Epoch %d\tBatch %d\tTrain Loss: %.3f\tTrain Accuracy: %.3f" % (epoch, 
+                        counter, loss / float(counter), acc / float(counter))
+            
+            # Add train loss, train acc to data
+            train_loss[epoch], train_acc[epoch] = loss / float(counter), acc / float(counter)
+
+            # Increment Epoch
+            sess.run(entity_net.epoch_increment)
+
+            # Validate every so often
+            if epoch % FLAGS.validate_every == 0:
+                val_loss_val, val_acc_val = do_eval(val_n, bsz, sess, entity_net, valS, valS_len, valQ, valA)
+                print "Epoch %d Validation Loss: %.3f\tValidation Accuracy: %.3f" % (epoch, val_loss_val, val_acc_val)
+                
+                # Add val loss, val acc to data 
+                val_loss[epoch], val_acc[epoch] = val_loss_val, val_acc_val
+
+                # Update best_val
+                if val_acc[epoch] > best_val:
+                    best_val = val_acc[epoch]
+
+                # Save Model
+                saver.save(sess, ckpt_dir + "model.ckpt", global_step=entity_net.epoch_step)
+                with open(ckpt_dir + "training_logs.pik", 'w') as f:
+                    pickle.dump((train_loss, train_acc, val_loss, val_acc), f)
+
+                # Early Stopping Condition
+                if best_val > FLAGS.validation_threshold:
+                    break
+        
+        # Test Loop
+        test_loss, test_acc = do_eval(test_n, bsz, sess, entity_net, testS, testS_len, testQ, testA)
+        
+        # Print and Write Test Loss/Accuracy
+        print "Test Loss: %.3f\tTest Accuracy: %.3f" % (test_loss, test_acc)
+        with open(ckpt_dir + "output.txt", 'w') as g:
+            g.write("Test Loss: %.3f\tTest Accuracy: %.3f\n" % (test_loss, test_acc))
+
+def do_eval(n, bsz, sess, entity_net, evalS, evalS_len, evalQ, evalA):
+    """Perform an Evaluation Epoch on the Given Data"""
+    eval_loss, eval_acc, eval_counter = 0.0, 0.0, 0
+    for start, end in zip(range(0, n, bsz), range(bsz, n, bsz)):
+        curr_eval_loss, curr_eval_acc = sess.run([entity_net.loss_val, entity_net.accuracy],
+                                                 feed_dict={entity_net.S: evalS[start:end],
+                                                            entity_net.S_len: evalS_len[start:end],
+                                                            entity_net.Q: evalQ[start:end],
+                                                            entity_net.A: evalA[start:end]})
+        eval_loss, eval_acc, eval_counter = eval_loss + curr_eval_loss, eval_acc + curr_eval_acc, eval_counter + 1
+    return eval_loss / float(eval_counter), eval_acc / float(eval_counter)
+    
+
+if __name__ == "__main__":
+    tf.app.run()
